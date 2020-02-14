7dfd70de54e278ac278d141f96d63aa4552017c9
fx_kirin <fx.kirin@gmail.com>
2020-02-14 13:36:06 +0900

    add kaniexpect

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,13 @@ macro_rules! expect {
         $option.expect(concat!(file!(), ":", line!(), "|", $literal))
     }};
 }
+
+#[macro_export]
+macro_rules! kaniformat {
+    ($option:expr) => {{
+        format!(concat!(file!(), ":", line!()))
+    }};
+    ($option:expr, $literal:expr) => {{
+        format!(concat!(file!(), ":", line!(), "|", $literal))
+    }};
+}
